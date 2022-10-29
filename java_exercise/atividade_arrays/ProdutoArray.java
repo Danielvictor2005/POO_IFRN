0439e4b3ffc1f253670a46f8fd41627ba15c3426
Danielvictor2005 <danielvictor2305@gmail.com>
2022-10-29 01:13:49 -0300

    Colocando em prática Arrays(Vetores)

--- java_exercise/atividade_arrays/ProdutoArray.java
+++ java_exercise/atividade_arrays/ProdutoArray.java
@@ -1,3 +1,49 @@
+import java.util.Scanner;
+
 public class ProdutoArray {
-  
+
+  private String[] produtosNomes;
+  private double[] precosProdutos;
+
+  private double totalValor;
+  private int tamanhoArray;
+
+  Scanner leitorX = new Scanner(System.in);
+
+  public void fazerCompras() {
+    difinirTamanhoArray();
+    inserirDados();
+    valorTota();
+  }
+
+  private int difinirTamanhoArray() {
+
+    System.out.println("Difina um tamanho do array: ");
+    tamanhoArray = leitorX.nextInt();
+
+    produtosNomes = new String[tamanhoArray];
+    precosProdutos = new double[tamanhoArray];
+
+    return tamanhoArray;
+  }
+
+  private void inserirDados() {
+
+    if (tamanhoArray != 0) {
+      for (int x = 0; x < tamanhoArray; x++) {
+        System.out.println("Insira o nome do produto e preço: ");
+        produtosNomes[x] = leitorX.next();
+        precosProdutos[x] = leitorX.nextDouble();
+      }
+    } else {
+      difinirTamanhoArray();
+    }
+  }
+
+  private void valorTota() {
+    for (int x = 0; x < tamanhoArray; x++) {
+      totalValor += precosProdutos[x];
+    }
+    System.out.println("Valor total: " + totalValor);
+  }
 }
--- java_exercise/atividade_arrays/TesteProdutoArrays.java
+++ java_exercise/atividade_arrays/TesteProdutoArrays.java
@@ -1,5 +1,7 @@
 public class TesteProdutoArrays {
   public static void main(String[] args) {
-    
+    ProdutoArray p1 = new ProdutoArray();
+
+    p1.fazerCompras();
   }
 }
