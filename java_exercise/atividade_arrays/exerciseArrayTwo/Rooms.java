b48ddb76a1b50c89eae1e4fd7a27048a97a33a5e
Danielvictor2005 <danielvictor2305@gmail.com>
2022-11-11 22:40:20 -0300

    Fixing the code but having vector size issues

--- java_exercise/atividade_arrays/exerciseArrayTwo/Rooms.java
+++ java_exercise/atividade_arrays/exerciseArrayTwo/Rooms.java
@@ -2,53 +2,90 @@ import java.util.Scanner;
 
 public class Rooms {
 
-  private int[] rooms = new int[10];
-  private String[] emails = new String[10];
-  private String[] names = new String[10];
+  // VARIABLE AND OBJECT INITIALIZATION BLOCK
 
-  private int numberRoom = 0;
-
-  private int numberForStudents = 0;
-
+  // Inicialization of object Scanner
   Scanner leitorX = new Scanner(System.in);
+
+  // Three arrays
+  private int[] rooms = new int[3];
+  private String[] emails = new String[rooms.length];
+  private String[] names = new String[rooms.length];
+
+  // Two variables of control
+  private int numberRoom = 0;
+  private int numberForStudents = 0;
+  private String nameStudent;
+  private String emailStudent;
+
+  // SPECIAL METHODS BLOCK
 
   public void setRooms() {
 
-    System.out.println("How many students?");
-    numberForStudents = leitorX.nextInt();
-
-    while (numberForStudents < 1 || numberForStudents > 10) {
-      System.out.println("Excess number of students, please enter again: ");
-      numberForStudents = leitorX.nextInt();
-    }
+    setNumberOfStudents();
 
     for (int i = 0; i < numberForStudents; i++) {
 
-      System.out.printf("Rent %d", i + 1);
-
-      System.out.print("Name: ");
-      String nameStudent = leitorX.next();
-
-      System.out.println("Email: ");
-      String emailStudent = leitorX.next();
-
-      System.out.println("Room: ");
-      numberRoom = leitorX.nextInt();
+      System.out.printf("\nRent %d:\n", i + 1);
+      registerStudents();
 
       rooms[numberRoom] = numberRoom;
       names[numberRoom] = nameStudent;
       emails[numberRoom] = emailStudent;
+    }
+    showDataRooms();
+  }
 
+  private void registerStudents() {
+
+    System.out.print("Name: ");
+    nameStudent = leitorX.next();
+
+    System.out.print("Email: ");
+    emailStudent = leitorX.next();
+
+    System.out.print("Room: ");
+    numberRoom = leitorX.nextInt();
+
+    while (numberRoom >= rooms.length || numberRoom < 0) {
+      System.out.println("Enter a number valid again: ");
+      numberRoom = leitorX.nextInt();
+    }
+
+    while (rooms[numberRoom] != 0) {
+      System.out.println("This room is occupied, enter again: ");
+      numberRoom = leitorX.nextInt();
     }
   }
 
-  private void showDataRooms() {
-    System.out.println("Busy rooms: ");
-    for (int i = 0; i < rooms.length; i++) {
-      if (rooms[i] != 0) {
-        System.out.printf("%: %s, %s", numberRoom, names[i], emails[i]);
+  private void setNumberOfStudents() {
+
+    System.out.println("How many students?");
+    numberForStudents = leitorX.nextInt();
+
+    while (numberForStudents <= 0 || numberForStudents > rooms.length) {
+
+      if (numberForStudents == 0) {
+        System.out.println("Impossible to enter with zero students\nEnter with valid number again: ");
+        numberForStudents = leitorX.nextInt();
+      } else if (numberForStudents < 0) {
+        System.out.println("The number of students is less than 1\nEnter with valid number again: ");
+        numberForStudents = leitorX.nextInt();
+      } else {
+        System.out.println("The number of students exceeds the number of rooms\nEnter with valid number again: ");
+        numberForStudents = leitorX.nextInt();
       }
     }
   }
 
+  private void showDataRooms() {
+
+    System.out.println("Busy rooms: ");
+
+    for (int i = 0; i < rooms.length; i++) {
+      if (rooms[i] != 0) {
+        System.out.printf("%d: %s, %s\n", rooms[i], names[i], emails[i]);
+      }
+    }
+  }
 }
