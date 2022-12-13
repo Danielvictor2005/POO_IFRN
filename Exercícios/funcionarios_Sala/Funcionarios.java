7605433b79bb62d077e3f6d4edb294cecb278412
DanielVictor2005 <danielvictor2305@gmail.com>
2022-12-12 22:31:56 -0300

    Funcionário Sala

--- Exercícios/funcionarios_Sala/Funcionarios.java
+++ Exercícios/funcionarios_Sala/Funcionarios.java
@@ -2,69 +2,69 @@ import java.util.Scanner;
 
 public class Funcionarios {
 
-    private int id;
-    private String name;
-    private double salario;
-    private double taxa_aumento;
-    // private int quantidade_funcionarios;
+  private int id;
+  private String name;
+  private double salario;
+  private double taxa_aumento;
+  // private int quantidade_funcionarios;
 
-    Scanner leitorX = new Scanner(System.in);
+  Scanner leitorX = new Scanner(System.in);
 
-    public int getId() {
-        return id;
+  public int getId() {
+    return id;
+  }
+
+  private void setId() {
+    System.out.print("ID: ");
+    id = leitorX.nextInt();
+    leitorX.nextLine(); // ! Bug do Scanner de não ler o conteúdo seguinte
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  private void setName() {
+    System.out.print("\nName: ");
+    name = leitorX.nextLine();
+  }
+
+  public double getSalario() {
+    return salario;
+  }
+
+  private void setSalario() {
+    System.out.print("\nSalário: ");
+    salario = leitorX.nextDouble();
+  }
+
+  public double getTaxa() {
+    return taxa_aumento;
+  }
+
+  private void setTaxa() {
+    System.out.println("Digite a taxa de aumento: ");
+    taxa_aumento = leitorX.nextDouble();
+  }
+
+  public void aumento_Salario() {
+    System.out.println("Digite o ID do funcionário para o aumento: ");
+    if (leitorX.nextInt() == id) {
+      setTaxa();
+      salario = salario + (salario * taxa_aumento) / 100;
+    } else {
+      System.out.println("Não existe esse ID...");
     }
+  }
 
-    private void setId() {
-        System.out.print("ID: ");
-        id = leitorX.nextInt();
-        leitorX.nextLine(); // ! Bug do Scanner de não ler o conteúdo seguinte
-    }
+  public void cadastrar_Funcionario() {
+    setId();
+    setName();
+    setSalario();
+  }
 
-    public String getName() {
-        return name;
-    }
-
-    private void setName() {
-        System.out.print("\nName: ");
-        name = leitorX.nextLine();
-    }
-
-    public double getSalario() {
-        return salario;
-    }
-
-    private void setSalario() {
-        System.out.print("\nSalário: ");
-        salario = leitorX.nextDouble();
-    }
-
-    public double getTaxa() {
-        return taxa_aumento;
-    }
-
-    private void setTaxa() {
-        System.out.println("Digite a taxa de aumento: ");
-        taxa_aumento = leitorX.nextDouble();
-    }
-
-    public void aumento_Salario() {
-        System.out.println("Digite o ID do funcionário para o aumento: ");
-        if (leitorX.nextInt() == id) {
-            setTaxa();
-            salario = salario + (salario * taxa_aumento) / 100;
-        } else {
-            System.out.println("Não existe esse ID...");
-        }
-    }
-
-    public void cadastrar_Funcionario() {
-        setId();
-        setName();
-        setSalario();
-    }
-
-    public void mostrar_Dados() {
-        System.out.printf("%d, %s, %.2f \n", getId(), getName(), getSalario());
-    }
+  public void mostrar_Dados() {
+    System.out.printf("%d, %s, %.2f \n", getId(), getName(), getSalario());
+  }
 
 }
--- Exercícios/funcionarios_Sala/Teste_Funcionarios.java
+++ Exercícios/funcionarios_Sala/Teste_Funcionarios.java
@@ -1,14 +1,45 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 public class Teste_Funcionarios {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        Funcionarios fun = new Funcionarios();
-        ArrayList<Funcionarios> lista_Funcionarios = new ArrayList<>();
+    Funcionarios fun = new Funcionarios();
+    Funcionarios fun2 = new Funcionarios();
 
-        fun.cadastrar_Funcionario();
-        System.out.println(fun.getSalario());
-        lista_Funcionarios.add(fun);
+    ArrayList<Funcionarios> lista_Funcionarios = new ArrayList<Funcionarios>();
+    HashMap<Integer, Funcionarios> lista_Funcionarios2 = new HashMap<Integer, Funcionarios>();
+    LinkedList<Funcionarios> lista_Funcionarios3 = new LinkedList<Funcionarios>();
 
-    }
+    // TODO: ArrayList
+    // ArrayList é mais indicada para obter um objeto
+    lista_Funcionarios.add(fun);
+    lista_Funcionarios.add(fun2);
+
+    // TODO: HashMap
+    // É baseada em tabela empilhada, necessário uma chave ID e valor (null)
+    fun.cadastrar_Funcionario();
+
+    lista_Funcionarios2.put(10, fun);
+    lista_Funcionarios2.put(20, fun2);
+
+    // TODO: LinkedList
+    // É serve mais para inserção e exclusão de dados ou objeto, por ele ser em
+    // sequência.
+    lista_Funcionarios3.add(fun);
+
+    System.out.println(lista_Funcionarios2.get(10).getName());
+    // for (int i = 0; i < lista_Funcionarios.size(); i++) {
+    // lista_Funcionarios.get(i).cadastrar_Funcionario();
+    // }
+
+    // for (int i = 0; i < lista_Funcionarios.size(); i++) {
+    // System.out.println(lista_Funcionarios.get(i).getId());
+    // System.out.println(lista_Funcionarios.get(i).getName());
+    // System.out.println(lista_Funcionarios.get(i).getSalario());
+
+    // }
+
+  }
 }
