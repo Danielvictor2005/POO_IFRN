09184c4a43b4e0980450d114875574eb1bb10edc
DanielVictor2005 <danielvictor2305@gmail.com>
2022-12-22 23:20:18 -0300

    Nova atvidade sobre matrizes

---
+++ Exercícios/Matrizes/Questao1/One.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class One {
+  private int qLinhas; // Linhas
+  private int qColunas; // Colunas
+  private int numero;
+  private int matriz[][];
+
+  Scanner leitorX = new Scanner(System.in);
+
+  private void leituraDeDados() {
+    System.out.println("Digite um número para linhas: "); qLinhas = leitorX.nextInt();
+
+    System.out.println("Digite um número para colunas: ");
+    qColunas = leitorX.nextInt();
+
+    matriz = new int[qLinhas][qColunas];
+
+    for (int linha = 0; linha < qLinhas; linha++) {
+      for (int coluna = 0; coluna < qColunas; coluna++) {
+        System.out.printf("Digite os valores para a linha %d e coluna %d da matriz: ", linha, coluna);
+        matriz[linha][coluna] = leitorX.nextInt();
+      }
+    }
+  }
+
+  private void encontrarValor() {
+
+    System.out.println("Digite para verficar se existe no array");
+    numero = leitorX.nextInt();
+
+    for (int linha = 0; linha < qLinhas; linha++) {
+      for (int coluna = 0; coluna < qColunas; coluna++) {
+        if (numero == matriz[linha][coluna]) {
+          System.out.printf("\nPosição %d,%d \n", linha, coluna);
+
+          if (coluna > 0 && coluna < qColunas) {
+            System.out.printf("Esquerda: %d\n", matriz[linha][coluna - 1]);
+          }
+
+          if (coluna < qColunas - 1) {
+            System.out.printf("Direita: %d\n", matriz[linha][coluna + 1]);
+          }
+
+          if (linha > 0) {
+            System.out.printf("Acima: %d\n", matriz[linha - 1][coluna]);
+          }
+
+          if (linha < qLinhas - 1) {
+            System.out.printf("Abaixo: %d\n", matriz[linha + 1][coluna]);
+          }
+
+        }
+      }
+    }
+  }
+
+  // TODO: Executar todos os métodos anteriores
+  public void executarMetodos() {
+    leituraDeDados();
+    encontrarValor();
+  }
+}
---
+++ Exercícios/Matrizes/Questao1/Principal.java
@@ -0,0 +1,21 @@
+public class Principal {
+  public static void main(String[] args) {
+    One teste = new One();
+
+    teste.executarMetodos();
+
+    // double matrizTeste[][] = new double[2][4];
+
+    // for (int i = 0; i < 2; i++) {
+    // for (int j = 0; j < 4; j++) {
+    // matrizTeste[i][j] = Math.random();
+    // }
+    // }
+    // for (int i = 0; i < 2; i++) {
+    // for (int j = 0; j < 4; j++) {
+    // System.out.printf("%.2f \n", matrizTeste[i][j]);
+    // }
+    // }
+
+  }
+}
