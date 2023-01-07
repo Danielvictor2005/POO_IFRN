52b4635b23940056fe925bedda3f59b12995acbd
Daniel Victor <102811916+Danielvictor2005@users.noreply.github.com>
2023-01-06 22:11:26 -0300

    Delete Exercícios/Matrizes/Questao7 directory

--- Exercícios/Matrizes/Questao7/Aplicacao.java
+++
@@ -1,7 +0,0 @@
-public class Aplicacao {
-  public static void main(String[] args) {
-   int matrizInteiros[][] = new int[][] {{123,31,3}, {231,13,13},{32,3,25}}; 
-    Seven.setMatrizInteiro(matrizInteiros);
-    System.out.println(Seven.somaMatrizInteiros());
-  }
-}
