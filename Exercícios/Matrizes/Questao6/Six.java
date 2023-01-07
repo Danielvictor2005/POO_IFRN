2c4473d856e87114660a3b612998f8caa5bcaeed
DanielVictor2005 <danielvictor2305@gmail.com>
2023-01-06 22:10:21 -0300

    Bug fixes

---
+++ Exercícios/Matrizes/Questao6/Six.java
@@ -0,0 +1,19 @@
+public class Seven {
+
+  private static int matrizInteiros[][];
+  private static int soma;
+
+  public static void setMatrizInteiro(int[][] matrizInteiros) {
+    Seven.matrizInteiros = matrizInteiros;
+  }
+
+  public static int somaMatrizInteiros() {
+    for (int linha[] : matrizInteiros) {
+      for (int valor : linha) {
+        soma += valor;
+      }
+    }
+    return soma;
+  }
+
+}
